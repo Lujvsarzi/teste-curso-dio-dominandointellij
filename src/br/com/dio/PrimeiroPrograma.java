222f098835941c2b59bf50a4644272742ab7dcb4
Lujvsarzi <lusarzi@gmail.com>
2022-03-20 14:39:17 -0300

    correcao

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -3,21 +3,20 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
-    public static void main(String[] args) {
-        Gato gato = Gato();
+    public void main(String[] args) {
+        Gato gato = new Gato();
 
         System.out.println(gato);
 
-        Livro livro1 = new Livro(nome: "O problema dos 3 corpos", numPaginas: 300);
+
+        String nome;
+        Integer numPaginas;
+        Livro livro1 = new Livro(nome= "O Problema dos 3 corpos", numPaginas= 300);
         System.out.println(livro1);
 
-        /*int a = 5;
-        int b = 3;
-
-        System.out.println("Hello World " + (a+b));*/
     }
 
-    class Livro {
+    class Livro{
         private String nome;
         private Integer numPaginas;
 
